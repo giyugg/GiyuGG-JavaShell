ff0625d714a1fbb6b7eb67409b19103cf2e2a3f2
Kyle Acuna <134776227+giyugg@users.noreply.github.com>
2025-11-23 22:36:44 +0800

    added basic functions (lol i just added `help`)

--- src/MainShell.java
+++ src/MainShell.java
@@ -2,6 +2,25 @@ import java.util.Scanner;
 
 public class MainShell {
     public static void main(String[] args) {
-        System.out.println("Hi, this is a test.");
+        Scanner shell = new Scanner(System.in);
+
+        System.out.println("Welcome to GiyuGG Shell. Please type 'help' for the list of commands.");
+        System.out.print("GiyuGG_Shell_CMDLine>> ");
+        String cmdLine = shell.nextLine();
+
+        switch (cmdLine) {
+            case "help":
+                help();
+                break;
+        }
+    }
+
+    public static void help() {
+        System.out.println("────────────────────────────────────────────");
+        System.out.println("        GiyuGG_Shell — Command List");
+        System.out.println("────────────────────────────────────────────");
+        System.out.println("info                                 | Kyle's Information");
+        System.out.println("patch                                | Version Information");
+        System.out.println("exit                                 | Exit GiyuGG_Shell");
     }
 }
